8854d886b0e9058e47aa48bee2d5401cb64d65f6
IMSSAM <codingssam@gmail.com>
2016-03-04 17:33:09 +0900

    range, pageCount 필터 추가

--- public/angularjs/filters/customfilters.js
+++ public/angularjs/filters/customfilters.js
@@ -16,4 +16,31 @@ angular.module('customFilters', [])
 				return data;
 			}
 		};
+	})
+	.filter('range', function($filter) {
+		return function(data, page, size) {
+			if (angular.isArray(data) && angular.isNumber(page) && angular.isNumber(size)) {
+				var startIndex = (page - 1) * size;
+				if (data.length < startIndex) {
+					return [];
+				} else {
+					return $filter('limitTo')(data.splice(startIndex), size);
+				}
+			} else {
+				return data;
+			}
+		};
+	})
+	.filter('pageCount', function() {
+		return function(data, size) {
+			if (angular.isArray(data)) {
+				var results = [];
+				for (var i = 0; i < Math.ceil(data.length / size); i++) {
+					results.push(i);
+				}
+				return results;
+			} else {
+				return data;
+			}
+		};
 	});
